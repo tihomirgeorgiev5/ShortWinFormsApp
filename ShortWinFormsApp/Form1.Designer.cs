2afa1890eab96a3d4a463895b7c06dbeef8e6119
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-12-14 20:24:43 +0100

    added label, textbox and button on MyApp form_design

--- ShortWinFormsApp/Form1.Designer.cs
+++ ShortWinFormsApp/Form1.Designer.cs
@@ -28,18 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(323, 332);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(219, 179);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "My App";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
