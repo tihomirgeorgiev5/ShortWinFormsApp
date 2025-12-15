12221ee5fc422392c4a3006e3c2b48be8327340b
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-12-15 17:24:00 +0100

    change name of Label1

--- ShortWinFormsApp/Form1.Designer.cs
+++ ShortWinFormsApp/Form1.Designer.cs
@@ -47,9 +47,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(26, 19);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(89, 15);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "Enter some text";
             // 
             // textBox1
             // 
